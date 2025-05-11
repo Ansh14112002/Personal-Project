3f61ebceeda5ac232cef3d7d674920a57c654135
Ansh Srivastava <srivastavaansh171@gmail.com>
2025-05-11 21:10:24 +0530

    Ready for deployment

--- app.py
+++ app.py
@@ -177,4 +177,5 @@ def quiz():
     return render_template('quiz.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=8080)
